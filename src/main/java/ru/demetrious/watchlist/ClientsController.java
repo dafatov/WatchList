f29a9491b0353b1f1fd981df619fff16f1b3a4b0
dafatov <dafatov@yandex.ru>
2023-07-28 09:30:05 +0300

    Optimized imports

--- src/main/java/ru/demetrious/watchlist/ClientsController.java
+++ src/main/java/ru/demetrious/watchlist/ClientsController.java
@@ -1,15 +1,13 @@
 package ru.demetrious.watchlist;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.List;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/clients")
