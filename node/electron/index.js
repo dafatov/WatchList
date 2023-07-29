d93c7bde4ebb2bcb94a75921108ff256e4c31c07
dafatov <dafatov@yandex.ru>
2023-07-29 21:54:42 +0300

    Rework package stage to package server into electron

--- node/electron/index.js
+++ node/electron/index.js
@@ -1,10 +1,27 @@
 const {app, BrowserWindow} = require('electron');
+const {spawn} = require('child_process');
+const treeKill = require('tree-kill');
 
-app.on('ready', () =>
-  new BrowserWindow({
-    fullscreen: true,
-    autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  }).loadURL('http://localhost:8080/'));
+const url = 'http://localhost:8080/';
+let serverProcess;
+
+app.on('ready', () => {
+  serverProcess = spawn('java', ['-jar', './server/server.jar']);
+
+  startBrowser();
+});
+
+app.on('will-quit', () => treeKill(serverProcess?.pid));
+
+const startBrowser = () => {
+  fetch(url)
+    .then(() => new BrowserWindow({
+      title: 'WatchList',
+      fullscreen: true,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    }).loadURL(url))
+    .catch(() => setTimeout(() => startBrowser(), 250));
+};
