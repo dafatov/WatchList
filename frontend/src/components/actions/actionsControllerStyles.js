165bf650c4f36bfff92f32c4f5a51020a9576e40
dafatov <dafatov@yandex.ru>
2023-09-11 12:40:05 +0300

    Исправил съезжающие кнопки actions

--- frontend/src/components/actions/actionsControllerStyles.js
+++ frontend/src/components/actions/actionsControllerStyles.js
@@ -15,7 +15,7 @@ export const useStyles = makeStyles(theme => ({
   },
   dialActionsHovered: {
     backgroundColor: theme.palette.secondary.main,
-    position: 'fixed',
+    position: 'absolute',
     display: 'flex',
     flexDirection: 'column',
     zIndex: 100,
