0477abdfee4f47a76f0ec0aab0955a13a92f32e0
dafatov <dafatov@yandex.ru>
2023-09-25 12:17:43 +0300

    Enlarged the anime list font Edited indents calculation Refactored image creation

--- frontend/src/utils/image.js
+++ frontend/src/utils/image.js
@@ -2,13 +2,17 @@ const {createCanvas} = require('canvas');
 const {theme} = require('../styles/theme');
 
 export const createWatchingList = (title, animeList) => {
-  const r = 12;
-  const indent = 8;
-  const height72 = 64;
-  const heightAfterTitle = 2 * indent + height72;
   const titleFont = '72px sans-serif';
-  const listFont = '24px sans-serif';
-  const {w, h} = {w: getTextWidth(animeList, title, titleFont, listFont) + 8 * indent, h: heightAfterTitle + 4 * (animeList.length + 1) * indent};
+  const listFont = '48px sans-serif';
+  const r = 16;
+  const indent = 8;
+
+  const heightAfterTitle = 2 * indent + getTextHeight([title], titleFont);
+  const {w, h} = {
+    w: getMaxTextWidth(animeList, title, titleFont, listFont) + 8 * indent,
+    h: heightAfterTitle + getTextHeight(animeList, listFont) + 2 * (animeList.length + 2) * indent,
+  };
+
   const canvas = createCanvas(w, h);
   const context = canvas.getContext('2d');
 
@@ -17,30 +21,42 @@ export const createWatchingList = (title, animeList) => {
 
   context.fillStyle = theme.palette.primary.dark;
   context.font = titleFont;
-  context.fillText(title, (w - context.measureText(title).width) / 2, height72);
+  context.fillText(title, (w - context.measureText(title).width) / 2, getTextBaseLineHeight(title, titleFont) + indent);
 
   drawRoundRect(context, [2 * indent, heightAfterTitle], [w - 2 * indent, h - 2 * indent], r);
 
   context.fillStyle = theme.palette.primary.main;
   context.font = listFont;
-  // eslint-disable-next-line no-loops/no-loops
-  for (let i = 0; i < animeList.length; i++) {
-    context.fillText(animeList[i], 4 * indent, heightAfterTitle + 4 * (i + 1) * indent);
-  }
+  animeList.reduce((acc, anime) => {
+    context.fillText(anime, 4 * indent, acc + getTextBaseLineHeight(anime, listFont) + 2 * indent);
+
+    return acc + getTextHeight([anime], listFont) + 2 * indent;
+  }, heightAfterTitle);
 
   return canvas.toDataURL();
 };
 
-const getTextWidth = (animeList, title, titleFont, listFont) => {
-  const testCanvas = createCanvas(1, 1);
-  const testContext = testCanvas.getContext('2d');
-
-  testContext.font = titleFont;
-  return Math.max(testContext.measureText(title).width, ...animeList.map(anime => {
-    testContext.font = listFont;
-    return testContext.measureText(anime).width;
+const getMaxTextWidth = (animeList, title, titleFont, listFont) => testCanvas(context => {
+  context.font = titleFont;
+  return Math.max(context.measureText(title).width, ...animeList.map(anime => {
+    context.font = listFont;
+    return context.measureText(anime).width;
   }));
-};
+});
+
+const getTextBaseLineHeight = (text, font) => testCanvas(context => {
+  context.font = font;
+  return context.measureText(text).actualBoundingBoxAscent;
+});
+
+const getTextHeight = (textList, font) => testCanvas(context => {
+  context.font = font;
+  return textList.reduce((acc, text) => {
+    const metrics = context.measureText(text);
+
+    return acc + metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+  }, 0);
+});
 
 const drawRoundRect = (context, [x1, y1], [x2, y2], r) => {
   context.beginPath();
@@ -51,4 +67,11 @@ const drawRoundRect = (context, [x1, y1], [x2, y2], r) => {
   context.arcTo(x1, y1, x1 + r, y1, r);
   context.closePath();
   context.fill();
+};
+
+const testCanvas = callback => {
+  const testCanvas = createCanvas(1, 1);
+  const testContext = testCanvas.getContext('2d');
+
+  return callback?.(testContext);
 };
